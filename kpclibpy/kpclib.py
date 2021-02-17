e4c776e283e93de830244fc790e5bbe60df6fdad
Roger Ye <shugaoye@yahoo.com>
2021-02-17 22:58:02 +0800

    Added KPCLib.dll and SkiaSharp.dll

--- kpclibpy/kpclib.py
+++ kpclibpy/kpclib.py
@@ -2,17 +2,31 @@ import os
 import clr
 import sys
 
-# Set environment variables
-assembly_path = os.environ['HOME']+'/github/passxyz/KPCLib/KPCLib/bin/Debug/netstandard2.0/publish'
-sys.path.append(assembly_path)
+def interop_dll_path(dll_name):
+    # Unfrozen path
+    dll_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib', dll_name)
+    if os.path.exists(dll_path):
+        return dll_path
 
-clr.AddReference("KPCLib")
-clr.AddReference("SkiaSharp")
+    # Frozen path, dll in the same dir as the executable
+    dll_path = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), dll_name)
+    if os.path.exists(dll_path):
+        return dll_path
 
-def main():
+    try:
+        # Frozen path packed as onefile
+        dll_path = os.path.join(sys._MEIPASS, dll_name)
+        if os.path.exists(dll_path):
+            return dll_path
+    except Exception:
+        pass
+
+    raise Exception('Cannot find %s' % dll_name)
+
+clr.AddReference(interop_dll_path('KPCLib.dll'))
+clr.AddReference(interop_dll_path('SkiaSharp.dll'))
+
+if __name__ == '__main__':
 	from KeePassLib import PwDatabase, PwGroup
 	print(PwDatabase)
 	print(PwGroup)
-
-if __name__ == '__main__':
-    main()
