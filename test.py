f5e0107757ab9149a23d5885dc875f98ede544e9
Roger Ye <shugaoye@yahoo.com>
2021-02-17 23:05:44 +0800

    Added test.py

---
+++ test.py
@@ -0,0 +1,43 @@
+import os
+import kpclibpy
+
+from KeePassLib import PwDatabase, PwGroup 
+from KeePassLib.Serialization import IOConnectionInfo
+from KeePassLib.Keys import CompositeKey, KcpPassword
+from KeePassLib.Interfaces import IStatusLogger
+
+class ShowWarningsLogger(IStatusLogger):
+    __namespace__ = "KPCLibPy"
+    
+    def StartLogging(self, strOperation, bWriteOperationToLog):
+        return
+    def EndLogging(self):
+        return
+    def SetProgress(self, uPercent):
+        return True
+    def SetText(self, strNewText, lsType):
+        return True
+    def ContinueWork():
+        return True
+
+test_db_path = os.environ['HOME']+'/github/passxyz/KPCLib/KPCLib.xunit/utdb.kdbx'
+ioc = IOConnectionInfo.FromPath(test_db_path)
+TEST_DB_KEY = "12345"
+cmpKey = CompositeKey()
+cmpKey.AddUserKey(KcpPassword(TEST_DB_KEY))
+
+pwDb = PwDatabase()
+
+swLogger = ShowWarningsLogger()
+pwDb.Open(ioc, cmpKey, swLogger)
+
+def printEntry(kp):
+    for kp in entry.Strings:
+        print('{}={}'.format(kp.Key, kp.Value.ReadString()))
+
+pg = pwDb.RootGroup
+i = 0
+for entry in pg.Entries:
+    print('--- Item {} ---'.format(i))
+    printEntry(entry)
+    i = i + 1
